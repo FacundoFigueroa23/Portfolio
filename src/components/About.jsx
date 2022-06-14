b24b87bcf5de9a67a9305a75f33d10f067b4cd8f
facu <facu-figueroa-98@hotmail.com>
2022-06-14 14:39:24 -0300

    Add english about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,11 +6,9 @@ const es = ["Sobre Mi", "Soy una persona a la que le apasiona la tecnología, le
 "Descubrí el mundo de la programación mientras me encontraba estudiando una carrera en la que me toco cursar informática y la verdad fue una de las materias que más disfruté y en la que mejor me fue. Después me enteré del bootcamp de Soy Henry gracias a un amigo y luego de meses de mucho esfuerzo y sacrificio pude graduarme.", 
 "Actualmente me encuentro en búsqueda de mi primer trabajo en el que pueda iniciarme y desarrollarme como profesional."]
 
-const en = ["About Me"]
-
-// const en = ["I'm a person who is passionate about technology, like challenges, and above all, lerning and improving every day.", 
-// "I discovered the world of programming while I was studying a career in which I had to take computer science and the truth is that it was one of the subjects I enjoyed the most and in which I did the best. Then I found out about the Soy Henry bootcamp thanks to a friend and after months of hard work and sacrifice I was able to graduate.", 
-// "I'm currently looking for my first job in which I can start and grow as a professional."]
+const en = ["About Me", "I'm a person who is passionate about technology, likes challenges, and above all, learning and improving every day.", 
+"I discovered the world of programming while I was studying a career in which I had to take computer science and the truth is that it was one of the subjects I enjoyed the most and in which I did the best. Then I found out about the Soy Henry bootcamp thanks to a friend and after months of hard work and sacrifice I was able to graduate.", 
+"I'm currently looking for my first job in which I can start and develop as a professional."]
 
 export default function About({lang}) {
   return (
@@ -18,9 +16,9 @@ export default function About({lang}) {
       <h1 className={styles.title}>{lang ? es[0] : en[0]}</h1>
       <div className={styles.content}>
         <section className={styles.text}>
-          <p>{es[1]}</p>
-          <p>{es[2]}</p>
-          <p>{es[3]}</p>
+          <p>{lang ? es[1] : en[1]}</p>
+          <p>{lang ? es[2] : en[2]}</p>
+          <p>{lang ? es[3] : en[3]}</p>
         </section>
         <section className={styles.imgContainer}>
           <img src={Image} alt="Myself" />
