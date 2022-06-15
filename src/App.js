e66b286f3c20ac89cfe4a4a81d2b83654f6b0288
facu <facu-figueroa-98@hotmail.com>
2022-06-15 09:55:03 -0300

    Add scroll to top button

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Technologies from './components/Technologies';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
+import ScrollToTop from 'react-scroll-to-top'
+
 export default function App() {
   const [lang, setLang] = useState(true)
 
@@ -20,6 +22,8 @@ export default function App() {
   }, [lang])
   return (
     <div className='container'>
+      <ScrollToTop smooth style={{backgroundColor: 'black'}}
+      component={<img src='https://api.iconify.design/uiw:caret-up.svg?color=%23ffffff' alt='CaretUp' />}/>
       <Home lang={lang} changeLang={changeLang}/>
       <About lang={lang}/>
       <Technologies lang={lang}/>
