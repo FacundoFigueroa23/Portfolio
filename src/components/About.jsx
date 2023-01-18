e142b791b808f25484fd4c56db6cbb17b922b1a4
facu <facu-figueroa-98@hotmail.com>
2023-01-17 21:08:33 -0300

    Change about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,11 +3,11 @@ import Image from '../assets/Image.jpeg'
 import styles from './About.module.css'
 
 const es = ["Sobre Mi", "Soy una persona a la que le apasiona la tecnología, le gustan los desafíos, y sobre todo, aprender y mejorar cada día.", 
-"Descubrí el mundo de la programación mientras me encontraba estudiando una carrera en la que me toco cursar informática y la verdad fue una de las materias que más disfruté y en la que mejor me fue. Después me enteré del bootcamp de Soy Henry gracias a un amigo y luego de meses de mucho esfuerzo y sacrificio pude graduarme.", 
+"Descubrí el mundo de la programación mientras me encontraba estudiando mi anterior carrera (ingeniería ambiental). Después me enteré del bootcamp de 'Soy Henry' gracias a un amigo y luego de meses de mucho esfuerzo y sacrificio logré graduarme.", 
 "Actualmente me encuentro en búsqueda de mi primer trabajo en el que pueda iniciarme y desarrollarme como profesional."]
 
 const en = ["About Me", "I'm someone very passionate about technology, I love challenges, and above all, learning and improving every day.", 
-"I discovered the world of programming while I was studying a career in which I had to take computer science and the truth is that it was one of the subjects I enjoyed the most and in which I did the best. Then I found out about the 'Soy Henry bootcamp' thanks to a friend and after months of hard work and sacrifice I was able to graduate.", 
+"I discovered the world of programming while I was studying my previous career (environmental engineering). Then I found out about the 'Soy Henry' bootcamp thanks to a friend and after months of sacrifice and hard work I managed to graduate.", 
 "I'm currently looking for my first job in which I can start and develop as a professional."]
 
 export default function About({lang}) {
